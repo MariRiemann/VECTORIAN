aa00f007fa0a96755f54581ecad34d095c56cf86
vladoriginal <vladoriginal@gmail.com>
2020-10-19 23:07:12 +0300

    Simple wire to allow DRAGON MOVE

---
+++ Assets/DragonBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class DragonBehavior : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 8f;
+
+    private Vector2 move = Vector2.zero;
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+}
